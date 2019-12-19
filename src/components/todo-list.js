8a2f34149145411be1bca39a46e1fce05faf90f3
kirilllyzhenkoff <kirilllyzhenkoff@gmail.com>
2019-12-20 01:24:08 +0300

    summary

---
+++ src/components/todo-list.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TodoList = () => {
+    return (
+        <ul>
+        <li>Learn React</li>
+    <li>Get a job of my dream</li>
+    </ul>
+)
+};
+export default TodoList;
