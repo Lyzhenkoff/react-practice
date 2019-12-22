687c2c87755b74d86d085af19c33aa883feec148
kirilllyzhenkoff <kirilllyzhenkoff@gmail.com>
2019-12-22 13:15:05 +0300

    update

---
+++ build/precache-manifest.f79d6591196e7bcf2e00e5d437fe4efa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e64ff8abf518eac5a00887e25037b284",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f2eb109d447533e2f8b",
+    "url": "/static/js/2.a26439ca.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a26439ca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7302d29003ba3fbe9427",
+    "url": "/static/js/main.5866de49.chunk.js"
+  },
+  {
+    "revision": "16bf403f85a67a0fb936",
+    "url": "/static/js/runtime-main.12a6431d.js"
+  }
+]);
--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -2,20 +2,22 @@ import React from "react";
 
 import TodoListItem from './todo-list-item';
 
+import './todo-list.css';
+
 const TodoList = ({ todos }) => {
     const element = todos.map ((item) => {
 
         const { id, ...itemProps } = item;
 
         return (
-            <li key={id}>
+            <li key={id} className='list-group-item'>
                 <TodoListItem {...itemProps}/>
             </li>
         )
     });
     return (
 
-        <ul>
+        <ul className='list-group todo-list'>
             { element }
         </ul>
     )
