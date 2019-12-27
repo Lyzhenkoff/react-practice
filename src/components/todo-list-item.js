46d3f3acf0558b77c8f34fd9b01532e6026fb9f7
kirilllyzhenkoff <kirilllyzhenkoff@gmail.com>
2019-12-27 17:55:33 +0300

    summary

--- src/components/todo-list-item.js
+++ src/components/todo-list-item.js
@@ -4,6 +4,6 @@ const TodoListItem = ({ label , important = false}) => {
     const style = {
         color: important ? 'tomato' : 'black'
     };
-    return <span style ={style}>{ label }</span>
+    return <span className='search' style ={style}>{ label }</span>
 }
 export default TodoListItem;
